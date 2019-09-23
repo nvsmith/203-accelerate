514a1632cf7e0bb617de34d8281e84165c43f403
Nathan Smith <nathanielvs@gmail.com>
2019-09-22 18:03:13 -0600

    Add Advance Custom Fields Plugin. Add content to single case studies

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -11,12 +11,47 @@ get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+			<?php while (have_posts()) : the_post();
+	      $services = get_field('services');
+	      $client = get_field('client');
+	      $site_link = get_field('site_link');
+	      $image_1 = get_field('image_1');
+	      $image_2 = get_field('image_2');
+	      $image_3 = get_field('image_3');
+				$size = "full";
+      ?>
+
+				<article class="case-study">
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+
+						<h4><?php echo $services; ?></h4>
+
+						<h4><?php echo $client ?></h4>
+
+						<?php the_content(); ?>
+
+						<p class="read-more-link">
+							<a href="<?php echo $site_link; ?>">Visit Live Site ›</a>
+						</p>
+					</aside>
+				</article>
+
+				<div class="case-study-images">
+					<?php if($image_1) {
+						echo wp_get_attachment_image( $image_1, $size );
+					}?>
+
+					<?php if($image_2) {
+						echo wp_get_attachment_image( $image_2, $size );
+					}?>
+
+					<?php if($image_3) {
+						echo wp_get_attachment_image( $image_3, $size );
+					}?>
+				</div><!-- end case-study-images -->
+			<?php endwhile; // end of the loop.?>
 		</div><!-- .main-content -->
-
-
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
