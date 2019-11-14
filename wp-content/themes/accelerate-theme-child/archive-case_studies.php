7dcb90cdbc3c764992a947f42f6adbc110ab0101
Nathan Smith <nathanielvs@gmail.com>
2019-11-14 08:42:21 -0700

    Add arhive single page links to landing page images

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -32,9 +32,12 @@
  				</article>
 
  				<div class="case-study-images">
- 					<?php if($image_1) {
- 						echo wp_get_attachment_image( $image_1, $size );
- 					}?>
+					<a href="<?php the_permalink(); ?>">
+						<?php if($image_1) {
+							echo wp_get_attachment_image( $image_1, $size );
+						} ?>
+					</a>
+
  				</div><!-- end case-study-images -->
  			<?php endwhile; // end of the loop.?>
  		</div><!-- .main-content -->
