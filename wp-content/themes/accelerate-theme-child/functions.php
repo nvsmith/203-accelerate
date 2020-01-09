39de40978c21aa86467a89ff8a2952cf36e606e4
Nathan Smith <nathanielvs@gmail.com>
2020-01-08 19:25:30 -0700

    Add custom template About & custom post type Services

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -20,8 +20,7 @@ function accelerate_child_scripts()
 
 add_action('wp_enqueue_scripts', 'accelerate_child_scripts');
 
-function create_custom_post_types()
-{
+function create_custom_post_types(){
     register_post_type(
       'case_studies',
       array(
@@ -34,6 +33,19 @@ function create_custom_post_types()
       'rewrite' => array( 'slug' => 'case-studies' ),
     )
   );
+
+  register_post_type(
+    'services',
+    array(
+    'labels' => array(
+    'name' => __('Services'),
+    'singular_name' => __('Service')
+    ),
+    'public' => true,
+    'has_archive' => true,
+    'rewrite' => array( 'slug' => 'services' ),
+  )
+);
 }
 add_action('init', 'create_custom_post_types');
 
