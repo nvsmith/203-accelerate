ce9c283c08db08810bfa1001ac679b1c4d2e56b5
Nathan Smith <nathanielvs@gmail.com>
2020-01-12 12:08:40 -0700

    Remove excess copied code from about-page template

--- wp-content/themes/accelerate-theme-child/custom-template/about.php
+++ wp-content/themes/accelerate-theme-child/custom-template/about.php
@@ -25,53 +25,39 @@
  <section class="about-page">
 	 <div class="site-content">
 		 <div class="service-offers">
-			 <?php query_posts('post_type=service_offers'); ?>
 
-			 <?php while (have_posts()) : the_post();
-	 			 $image_1 = get_field('image_1');
-	 			 $size = "full";
-	 		 ?>
-			 <section class="service-offers">
-				 <aside class="serivce-offers-sidebar">
-					 <h3><?php the_title(); ?></h3>
+		 <?php query_posts('post_type=service_offers'); ?>
+
+		 <?php while (have_posts()) : the_post();
+       $image_1 = get_field('image_1');
+       $size = "full";
+      ?>
+
+			 <section class="service-content">
+				 <aside class="service-offers-sidebar">
+					 <h3>
+						 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					 </h3>
+
 					 <p><?php the_content(); ?></p>
-				 </aside>
-			 </section>
+				 </aside> <!-- end .service-offers-sidebar -->
+			 </section> <!-- end .service-content -->
 
+			 <ul class="about-page-images">
+				 <li class="service-images">
+					 <figure>
+						 <a href="<?php the_permalink(); ?>">
+							 <?php echo wp_get_attachment_image($image_1, $size) ?>
+						 </a>
+					 </figure>
+				 </li>
+			 </ul><!-- end .about-page-images -->
+		 <?php endwhile; ?>
 
-			 <?php endwhile; ?>
+		 <?php wp_reset_query(); ?>
 
-			 <?php wp_reset_query(); ?>
-		 </div>
-	 </div>
- </section>
-
-  <div id="primary" class="site-content sidebar">
- 	 <div class="main-content" role="main">
- 		 <?php while (have_posts()) : the_post();
- 			 $service_offer = get_field('service_offer');
- 			 $back_link = get_field('back_link');
- 			 $image_1 = get_field('image_1');
- 			 $size = "full";
- 		 ?>
-
- 			 <article class="case-study">
- 				 <aside class="case-study-sidebar">
- 					 <h2><?php the_title(); ?></h2>
- 					 <?php the_content(); ?>
- 					 <p class="read-more-link">
- 						 <a href="<?php echo $back_link; ?>">&lsaquo; Go Back</a>
- 					 </p>
- 				 </aside>
- 			 </article>
-
- 			 <div class="case-study-images">
- 				 <?php if ($image_1) {
- 				 echo wp_get_attachment_image($image_1, $size);
- 		 }?>
- 			 </div><!-- end case-study-images -->
- 		 <?php endwhile; // end of the loop.?>
- 	 </div><!-- .main-content -->
-  </div><!-- #primary -->
+		 </div> <!-- end .service-offers -->
+	 </div> <!-- end .site content -->
+ </section> <!-- end .about-page -->
 
  <?php get_footer(); ?>
