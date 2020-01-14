92237477a9997ee0b81a0d3eb8eee1dfb20eda44
Nathan Smith <nathanielvs@gmail.com>
2020-01-13 20:34:34 -0700

    Add tagline to About Page

--- wp-content/themes/accelerate-theme-child/custom-template/about.php
+++ wp-content/themes/accelerate-theme-child/custom-template/about.php
@@ -18,53 +18,65 @@
 
  <div id="primary" class="home-page hero-content">
  	<div class="main-content" role="main">
+		<?php query_posts('post_type=banners'); ?>
+			<?php while (have_posts()) : the_post();
+				$hero_banner = get_field('hero_banner');
+			?>
 
-		<?php query_posts('post_type=banners&p=47441'); ?>
+			<h3 class="banner-content"><?php echo $hero_banner; ?></h3>
 
-		<?php while (have_posts()) : the_post();
-			$hero_banner = get_field('hero_banner');
-		?>
-
-		<h3 class="banner-content"><?php echo $hero_banner; ?></h3>
-
-		<?php endwhile; ?>
-
+			<?php endwhile; ?>
 		<?php wp_reset_query(); ?>
  	</div><!-- .main-content -->
  </div><!-- #primary -->
 
  <section class="about-page">
+	 <div class="tagline-wrapper">
+		 <div class="tagline-content">
+			 <?php query_posts('post_type=banners'); ?>
+ 				<?php while (have_posts()) : the_post();
+ 					$tagline_header = get_field('tagline_header');
+					$page_tagline = get_field('page_tagline');
+ 				?>
+
+ 				<h4 class="tagline-header"><?php echo $tagline_header; ?></h4>
+
+				<p id="tagline"><?php echo $page_tagline; ?></p>
+
+ 				<?php endwhile; ?>
+ 			<?php wp_reset_query(); ?>
+		</div><!-- end .tagline-content -->
+	 </div> <!-- end .tagline-wrapper -->
+
 	 <div class="site-content">
 		 <div class="service-offers">
-
 			 <?php query_posts('post_type=service_offers'); ?>
+				 <?php while (have_posts()) : the_post();
+		       $image_1 = get_field('image_1');
+		       $size = "full";
+		      ?>
 
-			 <?php while (have_posts()) : the_post();
-	       $image_1 = get_field('image_1');
-	       $size = "full";
-	      ?>
+				 <section class="service-content">
+					 <aside class="service-offers-sidebar">
+						 <h3>
+							 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						 </h3>
 
-			 <section class="service-content">
-				 <aside class="service-offers-sidebar">
-					 <h3>
-						 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					 </h3>
+						 <p><?php the_content(); ?></p>
+					 </aside> <!-- end .service-offers-sidebar -->
+				 </section> <!-- end .service-content -->
 
-					 <p><?php the_content(); ?></p>
-				 </aside> <!-- end .service-offers-sidebar -->
-			 </section> <!-- end .service-content -->
+				 <div class="about-page-images">
+					 <div class="service-images">
+						 <figure>
+							 <a href="<?php the_permalink(); ?>">
+								 <?php echo wp_get_attachment_image($image_1, $size) ?>
+							 </a>
+						 </figure>
+					 </div> <!-- end .service-images -->
+				 </div> <!-- end .about-page-images -->
 
-			 <div class="about-page-images">
-				 <div class="service-images">
-					 <figure>
-						 <a href="<?php the_permalink(); ?>">
-							 <?php echo wp_get_attachment_image($image_1, $size) ?>
-						 </a>
-					 </figure>
-				 </div> <!-- end .service-images -->
-			 </div> <!-- end .about-page-images -->
-
-			 <?php endwhile; ?>
+				 <?php endwhile; ?>
 			 <?php wp_reset_query(); ?>
 		 </div> <!-- end .service-offers -->
 	 </div> <!-- end .site content -->
